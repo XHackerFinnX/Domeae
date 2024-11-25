c45cfad673eb025c7d11a4a0db45245355f1b098
xhackerfinnx <hfalex120@mail.ru>
2024-11-25 19:02:25 +0300

    создание декоратора. в процессе

---
+++ models/notifications.py
@@ -0,0 +1,10 @@
+import asyncio
+
+
+async def notifications_task(func):
+    
+    async def wrapper(chat_id: int, username: str, case_text: str, text_task: str):
+        
+        pass
+        
+    return wrapper
